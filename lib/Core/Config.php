1d102fcd33bec76c37e1f7fc61fec0490ec981cd
wangwei <434533508@qq.com>
2018-12-23 11:48:00 +0800

    @

--- lib/Core/Config.php
+++ lib/Core/Config.php
@@ -5,9 +5,9 @@ namespace Aliyun\Core;
 use Aliyun\Core\Regions\EndpointConfig;
 
 //config http proxy
-define('ENABLE_HTTP_PROXY', FALSE);
-define('HTTP_PROXY_IP', '127.0.0.1');
-define('HTTP_PROXY_PORT', '8888');
+@define('ENABLE_HTTP_PROXY', FALSE);
+@define('HTTP_PROXY_IP', '127.0.0.1');
+@define('HTTP_PROXY_PORT', '8888');
 
 
 class Config
